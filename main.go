64a34d709c6089abf72c005712d2518e28c842f4
Vadim Glazov <v.glazov@f6.ru>
2025-11-16 16:13:01 +0300

    sub

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("This code reads 2 digits from os.Stdin and writes their sum to os.Stdout\na: ")
+	fmt.Print("This code reads 2 digits from os.Stdin and writes their sum and sub to os.Stdout\na: ")
 	var a, b int
 	fmt.Scanf("%d", &a)
 	fmt.Print("b: ")
 	fmt.Scanf("%d", &b)
 	fmt.Println("Sum: ", a+b)
+	fmt.Println("Sub: ", a-b)
 }
